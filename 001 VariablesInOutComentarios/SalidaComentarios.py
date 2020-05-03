abb6b0f8285c21c4e96f12af44ca772e7f7f1164
Miguel Alcaide <malcabaut@gmail.com>
2020-05-03 21:36:55 +0200

    Salidas entradas y variables.

--- 001 VariablesInOutComentarios/SalidaComentarios.py
+++ 001 VariablesInOutComentarios/SalidaComentarios.py
@@ -3,4 +3,4 @@ print ("Hola mundo")
 """ Este es un comentario multilinea.
 No se por que Visual Studio Code me lo pinta como string...
 ¿Que esta pasando aqui?
- """ 
+ """
---
+++ 001 VariablesInOutComentarios/VariablesInOut.py
@@ -0,0 +1,8 @@
+#Solicita la edad de una persona y mente en un String.
+edad = input ("¿Cual es tu edad? ")
+print ("Asi que tiene "+edad+" años.")
+#Con un txt guardado con UTF-8 se ve correctamente las ñ.
+
+#Probamos a traformar el String en numero y le suma 10
+num = int(edad)+10
+print ("Le vamos a sumar 10: "+str (num)+"")
