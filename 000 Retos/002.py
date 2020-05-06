5d8bfa8b1003f9c46cf82f6841b966b72376f1a2
Miguel Alcaide <malcabaut@gmail.com>
2020-05-06 22:58:29 +0200

    Empezado reto del Tema3

---
+++ 000 Retos/002.py
@@ -0,0 +1,7 @@
+# Reto propuesto por el curso que estoy siguiendo de Structuralia (https://sando.aulapharos.com/).
+# Tema3 Elementos de control (condicionales y bucles)
+'''
+Realiza un programa que realice una cuenta atrás de una cantidad de tiempo (minutos y segundos) introducida por el usuario.
+Dicho programa deberá contar cuántos minutos quedan a no ser que quede menos de un minuto, que contará segundo a segundo.
+Así mismo, deberá obligar al usuario a introducir un número como input.
+'''
