4dff0d6b295db14f6907146f3020048ca6402d7e
Miguel Alcaide <malcabaut@gmail.com>
2020-05-12 23:19:37 +0200

    Ojenado w3schools

---
+++ w3schools/Dia 002/001_Multilinea.py
@@ -0,0 +1,19 @@
+print("""
+Con diez cañones por banda,
+Viento en popa, a toda vela,
+No corta el mar, sino vuela
+Un velero bergantín:
+Bajel pirata que llaman
+Por su bravura el Temido,
+En todo el mar conocido
+Del uno al otro confín.
+La luna en el mar rïela,
+En la lona gime el viento,
+Y alza en blando movimiento
+Olas de plata y azul;
+Y ve el capitán pirata,
+Cantando alegre en la popa,
+Asia a un lado, a otro Europa,
+Y allá a su frente Estambul
+""")
+print("Multilinea\nPrueba")
---
+++ w3schools/Dia 002/002_ChequearString.py
@@ -0,0 +1,5 @@
+test = "En un lugar de la mancha de cuyo nombre no quiero acordarme no ha mucho tiempo que vivía un hidalgo"
+x = "lugar" in test # devuelve true si encuentra el str
+print(x)
+x = "lkjh" not in test # devuelve true si no encuentra el str
+print(x)
