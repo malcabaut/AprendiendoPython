e8f157432ec2b2961a4477d3a89da89f0c236af8
Miguel Alcaide <malcabaut@gmail.com>
2020-06-13 18:42:37 +0200

    Empezando con djando

---
+++ 100 Django/django001.py
@@ -0,0 +1,5 @@
+'''
+Estoy trabajando con Visual Studio Code
+Uso el siguiente comando para crear el proyecto
+C:\Users\usuario\AppData\Local\Programs\Python\Python38-32\Scripts\django-admin.exe startproject Proyecto1
+'''            
