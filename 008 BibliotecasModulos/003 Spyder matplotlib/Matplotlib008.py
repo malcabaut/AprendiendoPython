47822a45d82ff86c580d9846cfaa89771610ca27
Miguel Alcaide <malcabaut@gmail.com>
2020-06-20 11:15:35 +0200

    Fin de curso de aulapharos

--- 008 BibliotecasModulos/003 Spyder matplotlib/Matplotlib008.py
+++ 008 BibliotecasModulos/003 Spyder matplotlib/Matplotlib008.py
@@ -1,7 +1,13 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+
 x=np.random.rand(80) 
 y=np.random.rand(80) 
+
 plt.scatter(x,y, c=range(80)) 
 plt.show() 
+
+plt.figure(1)
+plt.scatter(X_test[:, 0], X_test[:, 1]) 
+plt.show()
  
