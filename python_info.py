b1e8731f1614b08b5ace1b7d1ecbeb041b21f28b
Miguel Alcaide <malcabaut@gmail.com>
2020-06-29 22:03:05 +0200

    CAmbio de import

--- python_info.py
+++ python_info.py
@@ -1,14 +1,21 @@
 # Creo este script para ver la version de Python y los modulos
 import sys
-import pkg_resources
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
 
 #Nos muestra 
 print ("Mi version de Python es:  %s" % (sys.version))
-print ("Modulos de Python: ")
-dists = [d for d in pkg_resources.working_set]
 
-for i in dists:
-    print (i)
+
+dists = importlib_metadata.distributions()
+print ("Modulos de Python: ")
+for dist in dists:
+    name = dist.metadata["Name"]
+    version = dist.version
+    license = dist.metadata["License"]
+    print(f'{name} = {version}')
 '''
 luego instalo virtualenv (pip install virtualenv) para trabajar con distinta versionesde django 
 ya que los curso que estoy realizado esta en distintas versiones. 
